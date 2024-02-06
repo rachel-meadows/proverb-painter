d603a55fe7e9e4669e7cdc4b41e38ebdb6c3ca1d
Rachel Meadows <rachelnewzealand@gmail.com>
2024-02-06 21:32:28 +1300

    Set up new ASP.Net MVC template

---
+++ ProverbPainter/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProverbPainter.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
