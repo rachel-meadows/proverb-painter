215d6ec5d18ad76a003711992af996b00f1fd6b9
Rachel Meadows <rachelnewzealand@gmail.com>
2024-04-02 20:29:12 +1300

    Add basic canvas

--- ProverbPainter/Scripts/Game.js
+++ ProverbPainter/Scripts/Game.js
@@ -1,3 +1,48 @@
-function main() {}
+window.addEventListener(
+  'load',
+  function () {
+    var canvas = document.getElementById('canvas');
+    var context = canvas.getContext('2d');
+    var isIdle = true;
+    function drawstart(event) {
+      context.beginPath();
+      context.moveTo(
+        event.pageX - canvas.offsetLeft,
+        event.pageY - canvas.offsetTop
+      );
+      isIdle = false;
+    }
+    function drawmove(event) {
+      if (isIdle) return;
+      context.lineTo(
+        event.pageX - canvas.offsetLeft,
+        event.pageY - canvas.offsetTop
+      );
+      context.stroke();
+    }
+    function drawend(event) {
+      if (isIdle) return;
+      drawmove(event);
+      isIdle = true;
+    }
+    function touchstart(event) {
+      drawstart(event.touches[0]);
+    }
+    function touchmove(event) {
+      drawmove(event.touches[0]);
+      event.preventDefault();
+    }
+    function touchend(event) {
+      drawend(event.changedTouches[0]);
+    }
 
-document.addEventListener('DOMContentLoaded', main);
+    canvas.addEventListener('touchstart', touchstart, false);
+    canvas.addEventListener('touchmove', touchmove, false);
+    canvas.addEventListener('touchend', touchend, false);
+
+    canvas.addEventListener('mousedown', drawstart, false);
+    canvas.addEventListener('mousemove', drawmove, false);
+    canvas.addEventListener('mouseup', drawend, false);
+  },
+  false
+);
