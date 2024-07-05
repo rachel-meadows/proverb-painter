16ce6de43e6c50d4d295a7798bcc96fc42c6431a
Rachel Meadows <rachelnewzealand@gmail.com>
2024-07-06 10:56:28 +1200

    Add database integration to SQL Server

---
+++ proverb-painter.Server/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using proverb_painter.Server.Entities;
+
+namespace proverb_painter.Server.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Player> Players { get; set; }
+    }
+}
